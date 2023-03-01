0fd110d78eaae3372172f3ce3684d13fad851f37
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2023-03-01 17:24:43 -0300

    removing unused controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { EmployeesModule } from './employees/employees.module';
@@ -7,7 +6,6 @@ import { WorkdaysModule } from './workdays/workdays.module';
 
 @Module({
   imports: [PrismaModule, EmployeesModule, WorkdaysModule],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
